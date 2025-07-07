6057d01671809d189d406ed984845df61b91d219
Md-Kayum <kayum4125@gmail.com>
2025-07-07 23:14:51 +0600

    Updated  Course_catalougeController.java

--- src/pkg223061008_project/techmentor/Course_catalougeController.java
+++ src/pkg223061008_project/techmentor/Course_catalougeController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package pkg223061008_project.techmentor;
 
 import java.net.URL;
@@ -9,13 +5,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
-/**
- * FXML Controller class
- *
- * @author Apollo Gadget
- */
 public class Course_catalougeController implements Initializable {
 
     @FXML
@@ -27,52 +19,40 @@ public class Course_catalougeController implements Initializable {
     @FXML
     private Button EnrollButtonpyth;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
-    private void EnrollHTML(ActionEvent event) {
-            showEnrollConfirmation("HTML Basics");
-
-    }
-
-    private void EnrollPython(ActionEvent event) {
-            showEnrollConfirmation("HTML Basics");
-
-    }
-
-    private void EnrollNet(ActionEvent event) {
-            showEnrollConfirmation("HTML Basics");
-
-    }
-    private void showEnrollConfirmation(String courseName) {
-    System.out.println("Enrolled in: " + courseName);
-
-    javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
-    alert.setTitle("Enrollment Successful");
-    alert.setHeaderText(null);
-    alert.setContentText("You have successfully enrolled in: " + courseName);
-    alert.showAndWait();
-}
-
-    @FXML
-    private void handleEnrollNet(ActionEvent event) {
+        // Any future initialization logic goes here
     }
 
     @FXML
     private void HandleHTMLEnroll(ActionEvent event) {
+        showEnrollConfirmation("HTML Basics");
     }
 
     @FXML
     private void HandlePytEnroll(ActionEvent event) {
+        showEnrollConfirmation("Python for Beginners");
+    }
+
+    @FXML
+    private void handleEnrollNet(ActionEvent event) {
+        showEnrollConfirmation("Networking Fundamentals");
     }
 
     @FXML
     private void HandlecyberEnroll(ActionEvent event) {
+        showEnrollConfirmation("Introduction to Cybersecurity");
     }
 
+    private void showEnrollConfirmation(String courseName) {
+        System.out.println("Enrolled in: " + courseName);
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Enrollment Successful");
+        alert.setHeaderText(null);
+        alert.setContentText("You have successfully enrolled in: " + courseName);
+        alert.showAndWait();
+
+        
+    }
 }
