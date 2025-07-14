6d94cd597c96dff3b613a0214e1a25c0533842f0
Md-Kayum <kayum4125@gmail.com>
2025-07-14 14:04:35 -0600

    Added Session to track logged in users

---
+++ src/pkg223061008_project/techmentor/Session.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg223061008_project.techmentor;
+
+/**
+ *
+ * @author Apollo Gadget
+ */
+public class Session {
+    public static int loggedInUserId = -1;
+}
